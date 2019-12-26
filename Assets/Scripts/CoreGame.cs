827b3042a6ade1d369ade10c51da45c036ae9232
Maarten <r0761272@student.thomasmore.be>
2019-12-26 16:31:15 +0100

    First commit

---
+++ Assets/Scripts/CoreGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoreGame : MonoBehaviour
+{
+    [SerializeField] int low, high;
+    [SerializeField] Text guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGame();
+    }
+
+    void startGame()
+    {
+        high++;
+        step();
+    }
+    int Guess()
+    {
+        return Random.Range(low, high);
+    }
+
+    void step()
+    {
+        guess.text = Guess().ToString();
+    }
+
+
+    public void higher()
+    {
+        low = Guess()-1;
+        step();
+    }
+
+    public void lower()
+    {
+        high = Guess()+1;
+        step();
+    }
+
+  
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int low, high;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGame();
+    }
+
+    void startGame()
+    {
+        high = 1000;
+        low = 1;
+        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Pick a number boii :)");
+        Debug.Log("Lowest dude is " + low);
+        Debug.Log("highest dude is " + high);
+        Debug.Log("Up is higher, Down is lower, Enter is correct");
+        step();
+    }
+    int Guess()
+    {
+        return (low + high) / 2;
+    }
+
+    void step()
+    {
+        Debug.Log("Is it lower or higher than " + Guess());  
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            low = Guess();
+            step();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            high = Guess();
+            step();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Ayyyyy ezzzz");
+     
+            startGame();
+        }
+    }
+}
